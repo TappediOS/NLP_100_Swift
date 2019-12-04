0fb6eaea542c7d61faf5109e7b9096087593fbd2
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-12-04 14:10:47 +0900

    File Setting done

---
+++ NLP_100_Swift/Chapter1.swift
@@ -0,0 +1,13 @@
+//
+//  Chapter1.swift
+//  NLP_100_Swift
+//
+//  Created by jun on 2019/12/04.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import Foundation
+
+struct Chapter1 {
+   
+}
---
+++ NLP_100_SwiftTests/Chapter1Test.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter1Test.swift
+//  NLP_100_SwiftTests
+//
+//  Created by jun on 2019/12/04.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import XCTest
+@testable import NLP_100_Swift
+
+class Chapter1Test: XCTestCase {
+   
+   func testQ1() {
+      
+   }
+}

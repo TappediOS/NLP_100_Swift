6af489c059e37148a85ff75a93afec8e5f111ccc
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-12-04 14:06:02 +0900

    Initial Commit

---
+++ NLP_100_Swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NLP_100_Swift
+//
+//  Created by jun on 2019/12/04.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+   func applicationDidFinishLaunching(_ aNotification: Notification) {
+      // Insert code here to initialize your application
+   }
+
+   func applicationWillTerminate(_ aNotification: Notification) {
+      // Insert code here to tear down your application
+   }
+
+
+}
+
---
+++ NLP_100_Swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NLP_100_Swift
+//
+//  Created by jun on 2019/12/04.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+
+      // Do any additional setup after loading the view.
+   }
+
+   override var representedObject: Any? {
+      didSet {
+      // Update the view, if already loaded.
+      }
+   }
+
+
+}
+

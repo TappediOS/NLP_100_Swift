27bcc9ca61357b2244bf24fe763bad9c3ccfd453
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-12-08 16:56:18 +0900

    add high temp.txt

---
+++ NLP_100_SwiftTests/Chapter2Test.swift
@@ -0,0 +1,63 @@
+//
+//  Chapter2Test.swift
+//  NLP_100_SwiftTests
+//
+//  Created by jun on 2019/12/08.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import Foundation
+@testable import NLP_100_Swift
+
+
+class Chapter2Test: XCTestCase {
+   
+   func testQ10() {
+      
+   }
+   
+   func testQ11() {
+      
+   }
+   
+   func testQ12() {
+     
+   }
+   
+   func testQ13() {
+     
+   }
+   
+   func testQ14() {
+      
+   }
+
+   func testQ15_1() {
+     
+   }
+   
+   func testQ15_2() {
+
+   }
+   
+   func testQ16_1() {
+     
+      
+   }
+   
+   func testQ16_2() {
+   
+   }
+   
+   func testQ17() {
+
+   }
+   
+   func testQ18() {
+    
+   }
+   
+   func testQ19() {
+
+   }
+}

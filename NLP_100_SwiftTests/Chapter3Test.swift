3c08b3022fbabcfcd3a0482b85c3f325feadd19d
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-12-11 15:13:22 +0900

    テストに使う関数の追加

--- NLP_100_SwiftTests/Chapter3Test.swift
+++ NLP_100_SwiftTests/Chapter3Test.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+import XCTest
+@testable import NLP_100_Swift
+
+class Chapter3Test: XCTestCase {
+   
+   func testQ20() {
+      
+   }
+   
+   func testQ21() {
+      
+   }
+   
+   func testQ22() {
+      
+   }
+   
+   func testQ23() {
+      
+   }
+   
+   func testQ24() {
+      
+   }
+   
+   func testQ25() {
+      
+   }
+   
+   func testQ26() {
+      
+   }
+   
+    
+   func testQ27() {
+      
+   }
+   
+   func testQ28() {
+      
+   }
+   
+   func testQ29() {
+      
+   }
+   
+}

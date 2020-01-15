154656e36ad5a6c549062c6bbba5b723505b0ce0
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-15 15:13:31 +0900

    morphの実装

---
+++ NLP_100_Swift/Chapter5/Morph.swift
@@ -0,0 +1,23 @@
+//
+//  Morph.swift
+//  NLP_100_Swift
+//
+//  Created by jun on 2020/01/15.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation
+
+class Morph {
+   var surface = ""
+   var base = ""
+   var pos = ""
+   var pos1 = ""
+   
+   init(surface: String, base: String, pos: String, pos1: String) {
+      self.surface = surface
+      self.base = base
+      self.pos = pos
+      self.pos1 = pos1
+   }
+}
